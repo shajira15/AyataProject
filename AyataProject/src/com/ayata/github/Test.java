63bf209bb774280ed13061580760c34f28019f99
user <user@LAPTOP-SPRD38BB>
2022-03-07 21:39:34 +0530

    First Git

---
+++ AyataProject/src/com/ayata/github/Test.java
@@ -0,0 +1,11 @@
+package com.ayata.github;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GITHUB Demo");
+
+	}
+
+}
---
+++ AyataProject/src/module-info.java
@@ -0,0 +1,2 @@
+module AyataProject {
+}
